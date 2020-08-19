cc0f37ae629bcd72add46b456653f916b71ece63
A Bond <a.rosicbond@gmail.com>
2020-08-19 20:07:20 +0100

    gitignore

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 // must restart server whenever you make changes in next.config
 module.exports = {
   env: {
-    MONGO_SRV: "<insert-mongo-srv>",
-    JWT_SECRET: "<insert-jwt-secret>",
-    CLOUDINARY_URL: "<insert-cloudinary-url>",
+    MONGO_SRV: "mongodb+srv://ABond:Porcelain1@soapybusiness.vn3qv.mongodb.net/soapybusiness?retryWrites=true&w=majority",
+    JWT_SECRET: "soapybusiness",
+    CLOUDINARY_URL: "qwertyuiop",
     STRIPE_SECRET_KEY: "<insert-stripe-secret-key>"
   }
 };
