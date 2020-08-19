6d6bc31b6ec76ad86f7cc61b8601681dda30d990
A Bond <a.rosicbond@gmail.com>
2020-08-19 16:10:53 +0100

    add products to cart

--- pages/product.js
+++ pages/product.js
@@ -7,7 +7,7 @@ function Product({ product, user }) {
   
   return (
     <>
-      <ProductSummary {...product}/>
+      <ProductSummary user={user} {...product}/>
       <ProductAttributes user={user} {...product}/>
     </>
   );
