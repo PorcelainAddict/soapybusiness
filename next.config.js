d867109211c7f44d9c44a9cc872c57ec13cd3db5
Alan F. Bond <57002969+PorcelainAddict@users.noreply.github.com>
2020-08-20 10:26:55 +0100

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-// must restart server whenever you make changes in next.config
-module.exports = {
-  env: {
-    MONGO_SRV: "mongodb+srv://ABond:Porcelain1@soapybusiness.vn3qv.mongodb.net/soapybusiness?retryWrites=true&w=majority",
-    JWT_SECRET: "soapybusiness",
-    CLOUDINARY_URL: "qwertyuiop",
-    STRIPE_SECRET_KEY: "<insert-stripe-secret-key>"
-  }
-};
