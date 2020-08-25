7ecc7004c1b3a9e080bef3b11253d32004b0da82
A Bond <a.rosicbond@gmail.com>
2020-08-25 12:10:08 +0100

    pagination

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,29 @@
 import React from 'react'
 import axios from 'axios'
 import ProductList from '../components/Index/ProductList';
+import ProductPagination from "../components/Index/ProductPagination";
 import baseUrl from '../utils/baseUrl';
 
-function Home({products}) {
-  return <ProductList products={products} />
+function Home({products, totalPages }) {
+  return (
+    <>
+     <ProductList products={products} />
+     <ProductPagination totalPages = {totalPages} />
+    </>
+  );
 }
 
-Home.getInitialProps = async() => {
+Home.getInitialProps = async ctx => {
+  const page = ctx.query.page ? ctx.query.page : "1"; 
+  const size = 8;
   //fetch data on server
   const url = `${baseUrl}/api/products`;
-    const response = await axios.get(url);
+  const payload = { params: { page, size }}
     
-    return { products: response.data }
+  const response = await axios.get(url, payload);
+
+    
+  return response.data 
   //return res data as an object
   // return { hello: 'planet'};
   // note: this object will be merged with existing props
