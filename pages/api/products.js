a26fa5bca76e25ea80ef9d1ac832e2eb433f2c53
A Bond <a.rosicbond@gmail.com>
2020-08-26 11:50:08 +0100

    sort working for user. Not for products

--- pages/api/products.js
+++ pages/api/products.js
@@ -16,7 +16,7 @@ export default async (req, res) => {
     products = await Product.find().limit(pageSize);
   } else {
     const skips = pageSize * (pageNum - 1)
-    products = await Product.find().skip(skips).limit(pageSize)
+    products = await Product.find().sort({ name: "asc" }).skip(skips).limit(pageSize)
   }
   // const products = await Product.find();
   res.status(200).json({products, totalPages});
